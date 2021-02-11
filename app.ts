438372b21118a61aea5d7590826c8239abda6e29
selwyn <selwyn.jacob316@gmail.com>
2021-02-11 15:31:44 -0500

    authentication cleanup

--- app.ts
+++ app.ts
@@ -11,54 +11,16 @@ const mongoose = require('mongoose');
 const cookieParser = require("cookie-parser");
 const jwt = require('express-jwt');
 const jsonwebtoken = require('jsonwebtoken');
-// const csrfProtection = csrf({
-//     cookie: true
-// });
 
 
 require('dotenv').config();
-// app.use(
-//     jwt({
-//       secret: process.env.JWT_SECRET,
-//       getToken: req => req.cookies.token,
-//       algorithms: ['HS256'] 
-//     })
-//   );
-// app.use(jwt({ secret: process.env.JWT_SECRET,}));
-// app.use(csrfProtection);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-// app.use((req, res, next) => {
-//     res.header({"Access-Control-Allow-Origin": "*"})
-//   }) 
-app.use('/api/stocks', require('./routes/stocks'));
-app.use('/api/auth', require('./routes/authentication'));
-
-
-
-// app.use(
-//     jwt({
-//       secret: process.env.JWT_SECRET,
-//       getToken: req => req.cookies.token,
-//         algorithms: ['HS256'] 
-//     })
-//   );
-
-//   app.post("/verify-token" , (req, res) => {
-//       console.log(req);
-//     let value = jwt.verify(req.cookies.token)
-//     return res.json(value);
-// })
-
 
 app.listen(port, () => {
     console.log(`App listeing at http://localhost:${port}`);
 })
-
-// app.get('/csrf-token', (req, res) => {
-//     res.json({ csrfToken: req.csrfToken() });
-// })
 
 app.use(express.json())
 app.use(
@@ -72,43 +34,8 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  return done(null, user);
-})
-
-passport.deserializeUser((user, done) => {
-  return done(null, user);
-})
-
-// const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-passport.use(new GoogleStrategy({
-    clientID: "445355911190-15pti9bl81t1qb08d876m0vn72qsls3m.apps.googleusercontent.com",
-    clientSecret: "5KyDcGaX39F5133ruLkcPZzE",
-    callbackURL: "/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-      console.log(profile)
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return cb(err, user);
-    // });
-    cb(null, profile);
-  }
-));
-
-app.get('/auth/google', 
-passport.authenticate('google', { scope: ['profile']}));
-
-app.get('/auth/google/callback', 
-  passport.authenticate( 'google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('http://localhost:3000/');
-  })
-
-app.get('/getuser', (req, res) => {
-  res.send(req.user);
-})
+app.use('/api/stocks', require('./routes/stocks'));
+app.use('/auth', require('./routes/authentication'));
 
 
 mongoose.connect(
--- routes/authentication.ts
+++ routes/authentication.ts
@@ -1,11 +1,52 @@
 import express from 'express';
-
+import passport from 'passport';
+import passportLocal from 'passport-local';
 const router = express.Router();
 const axios = require("axios").default
 const User = require("../models/Users");
 const bcrypt = require('bcryptjs');
 const jsonwebtoken = require('jsonwebtoken');
 
+const LocalStrategy = passportLocal.Strategy;
+
+passport.serializeUser((user, done) => {
+    return done(null, user);
+  })
+  
+  passport.deserializeUser((user, done) => {
+    return done(null, user);
+  })
+
+  passport.use(new LocalStrategy((username, password, done) => {
+      User.findOne({username: username}, (err, user: any) => {
+          if (err) throw err;
+          if(!user) return done(null, false);
+          bcrypt.compare(password, user.password, (err, result) => {
+              if(err) throw err;
+              if(result === true) {
+                  return done(null, user);                  
+              } else {
+                  return done(null, false);
+              }
+          })
+      })
+  }))
+  
+  const GoogleStrategy = require('passport-google-oauth20').Strategy;
+  
+  passport.use(new GoogleStrategy({
+      clientID: "445355911190-15pti9bl81t1qb08d876m0vn72qsls3m.apps.googleusercontent.com",
+      clientSecret: "5KyDcGaX39F5133ruLkcPZzE",
+      callbackURL: "/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log(profile)
+      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
+      cb(null, profile);
+    }
+  ));
 
 router.post("/register-user/", async (req, res) => {    
     // await User.create({email: req.params.email, password: req.params.password })
@@ -54,14 +95,27 @@ router.post("/logout/", async (req, res) => {
     res.cookie('token', 'none', { maxAge: 0, httpOnly: true})
     return res.json({message: "User logged out succesfully"})
 })
-const passport = require('passport');
 
-router.get("/google", passport.authenticate("google", {
-    scope: ['profile']
-}));
+router.get('/google', 
+passport.authenticate('google', { scope: ['profile']}));
 
-router.get("/google/redirect", (req, res) => {
-    console.log("here")
-    res.json({data: "success"})
+router.get('/google/callback', 
+  passport.authenticate( 'google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('http://localhost:3000/');
+  })
+
+  router.get('/getuser', (req, res) => {
+  res.send(req.user);
 })
+// const passport = require('passport');
+
+// router.get("/google", passport.authenticate("google", {
+//     scope: ['profile']
+// }));
+
+// router.get("/google/redirect", (req, res) => {
+//     console.log("here")
+//     res.json({data: "success"})
+// })
 module.exports = router; 
